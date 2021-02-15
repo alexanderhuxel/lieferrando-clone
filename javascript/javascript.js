4a265fc22b056f601fea26257de150eecebdc298
alexanderhuxel <alexanderhuxel@gmail.com>
2021-02-15 20:18:43 +0100

    large changes new structure, in changes in progress

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,56 +1,135 @@
-let heart = "fas fa-heart";
-let info = "fas fa-info";
-let search = "fas fa-search";
-let order = "past,pizza,burger,drinks";
+let Basket = [];
+let Food = [];
+let Summary = [];
+let data = JSON.parse(localStorage.getItem("Food"));
+
 
 
 
 /**
  * initiate all needed functions to 
  * display the homepage
- * @function getMeal(key,templatenumber)
- * @function getMeal(key,templatenumber)
- * @function getMeal(key,templatenumber)
- * @function getMeal(key,templatenumber)
+ * @event window.addEventListener("resize",  @function checkBasketVersion()
  */
 function init() {
-    getMeal("pasta", 0);
-    getMeal("pizza", 1);
-    getMeal("burger", 2);
-    getMeal("drinks", 3);
-    setInterval(() => {
-        checkBasketVersion();
-    }, 50);
+    fillArrayWithData();
+    getMeal();
+    checkBasketVersion();
+
 }
 
+/**
+ * set an item to the localstorage
+ * @param {string} key -- "keyword"
+ * @param {string} value --"value"
+ */
+function setArray(key, value) {
+    localStorage.setItem(key, JSON.stringify(value));
+}
+
+
+/**
+* load a Array out of the Localstorage
+* @param {string} key 
+*/
+function getArray(key) {
+    return JSON.parse(localStorage.getItem(key));
+}
+
+/**
+ * get data out of a JSON-file from a Server
+ * @function fetch(url)
+ * 
+ */
+
+function fillArrayWithData() {
+    fetch(
+        "../javascript/data.json")
+        .then(response => response.json())
+        .then(data => {
+            Food.push(...data.pasta);
+            Food.push(...data.pizza);
+            Food.push(...data.burger);
+            Food.push(...data.drinks);
+            localStorage.setItem('Food', JSON.stringify(Food));
+        }).catch((error) => {
+            console.error('Error:', error);
+        });;
+}
+
+let date = JSON.parse(localStorage.getItem("Food"));
+
+
+
+
+
+
+/**
+ * check the innerWidth 
+ * if is > 800 run
+ * @function setBasket()
+ * is is < 800 run
+ * @function setBasketMedia()
+ */
 function checkBasketVersion() {
+    window.addEventListener("resize", checkBasketVersion);
     if (window.innerWidth > 800) {
-        console.log("Desktop Version")
         setBasket();
     } if (window.innerWidth < 800) {
         setBasketMedia();
-        console.log("Mobile Version")
     }
 }
+
+/**
+ * change the innerHTML of "Basket"
+ * to the returned data of 
+ * @function getBasketMedia()
+ */
 
 function setBasketMedia() {
     document.getElementById("basket").innerHTML = getBasketMedia();
 }
 
+
+/**
+ * change the innerHTML of "Basket"
+ * to the returned data of 
+ * @function getBasket()
+ */
 function setBasket() {
     document.getElementById("basket").innerHTML = getBasket();
 }
 
+
+
+
+
+/**
+ * @returns
+ * the HTML for the Basket 
+ * MobileVersion
+ */
 function getBasketMedia() {
     return `   <span>
     <p id="basket-logo-media"><i class="fas fa-shopping-cart"></i></p>
     <p>1</p>
 </span>
 <span>
-    <p id="basket-summary-media">Warenkorb (8,00€)</p>
+    <p id="basket-summary-media">(${"Warenkorb " + Summary})</p>
 </span>`
 }
 
+
+
+
+
+
+/**
+ * @returns
+ * the HTML for the Basket 
+ * DesktopVersion
+ * with the @var Summary
+ */
 function getBasket() {
     return ` <h2>Warenkorb</h2>
     <span id="cart__description">
@@ -63,13 +142,19 @@ function getBasket() {
     </span>
     <span id="cart__row2">
         <p>Gesamt:</p>
-        <p>0,00€</p>
+        <p id="basket-summary">${Summary}</p>
     </span>
-    <p id="orderinicator">Leider kannst Du noch nicht bestellen. Pizza Parma liefert erst ab einem
+    <p id="Foodinicator">Leider kannst Du noch nicht bestellen. Pizza Parma liefert erst ab einem
         Mindestbestellwert von 18,00 €
         (exkl. Lieferkosten).</p>
     <button>Bestellen</button>`
 }
+
+
+
+
+
+
 
 document.addEventListener('scroll', () => {
     if (window.innerWidth > 800) {
@@ -85,39 +170,94 @@ document.addEventListener('scroll', () => {
     }
 });
 
-function getMeal(key, templatenumber) {
-    fetch("../javascript/data.json")
-        .then(response => response.json())
-        .then(data => {
-            let template = data.templates[templatenumber];
-            returnmealtemplate(template);
-            for (let i = 0; i < data[key].length; i++) {
-                let index = data[key][i]
-                document.getElementById("foodbox").innerHTML += returnmeal(index);
-            }
-        });
+function createNavigationBar() {
+    for (let i = 0; i < date[41][0].length; i++) {
+        document.getElementById("bar").innerHTML +=
+            `<p  ${data.templates[0][i].name}">${data.templates[0][i].name}</p>`
+    }
 }
 
 
-function returnmeal(index) {
+
+
+function createBasketItem(name,price) {
+    return {
+        "name": name,
+        "price": price
+    }
+}
+console.log()
+function addToBasket(i){
+    
+    Basket.push(createBasketItem(date[i].name,date[i].price));
+    console.log(Basket)
+    Summary.push(Basket[i].price);
+    console.log(Summary)
+
+}
+
+
+
+
+
+
+
+
+
+
+
+// Meal and Template
+// Meal and Template
+// Meal and Template
+// Meal and Template
+function getMeal() {
+    for (let i = 0; i < date.length; i++) {
+        if (date[i].category == undefined) {
+            document.getElementById("foodbox").innerHTML += returnmeal(i);
+        }
+        if (date[i].category == "template") {
+            getMealTemplate(i);
+        }
+    }
+}
+
+
+/**
+ * @returns the HTML for the Menu
+ * @param {string} nameOfArray 
+ * @param {number} numberOfArray 
+ * @param {number} i 
+ */
+function returnmeal(i) {
     return `<div class="meal">
     <div class="meal__name">
-        <div class="name">${index.name}</div>
-        <div class="add__button--cta"><i class="fas fa-plus"></i></div>
+    <div  class="name">${date[i].name}</div>
+    <div onclick="addToBasket(${i})" class="add__button--cta"><i  class="fas fa-plus"></i></div>
     </div>
     <div class="meal__discription">
-        <p>${index.ingredients}</p>
+    <p>${date[i].ingredients}</p>
     </div>
     <div class="meal__price">
-        <p>${index.price}</p>
+    <p>${date[i].price}</p>
     </div>
-</div>`
+    </div>`
 }
 
-function returnmealtemplate(template) {
-    document.getElementById("foodbox").innerHTML += `
-    <div class="meal__template" >
-    <img src="${template.img}">
-    <div class="meal__category">${template.name}</div>
-    <p class="meal__info">${template.discription}</p></div>`
+/**
+ * get the informations from @function returnMealtemplate();
+ * @param {number} templateNumber 
+ */
+function getMealTemplate(i) {
+    document.getElementById("foodbox").innerHTML += returnMealtemplate(i);
+}
+/**
+ * @returns the HTML for the Template of the Menu
+ * @param {number} templateNumber 
+ */
+function returnMealtemplate(i) {
+    return ` 
+    <div  class="meal__template" >
+    <img src="${date[i].img}">
+    <div class="meal__category">${date[i].name}</div>
+    <p class="meal__info">${date[i].discription}</p></div>`
 }
